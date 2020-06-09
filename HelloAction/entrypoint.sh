ef0c90400841929fd3f53cdbcd61a30f0380cdaa
Serhii <Koshelnyk>
2020-06-09 20:27:59 +0300

    upd output

--- HelloAction/entrypoint.sh
+++ HelloAction/entrypoint.sh
@@ -2,6 +2,6 @@
 
 echo "Hello $1"
 time=$(date)
-echo "::set-output name=surname::$monkey"
+echo "::set-output name=surname::monkey"
 echo "::set-output name=time::$time"
 echo "MY CUSTOM TEXT CAPS"
