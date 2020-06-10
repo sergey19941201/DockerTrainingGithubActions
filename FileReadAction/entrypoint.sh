328e809e9a169838f3ed9116f97a07410f0f4b00
Serhii <Koshelnyk>
2020-06-10 20:16:14 +0300

    read content

--- FileReadAction/entrypoint.sh
+++ FileReadAction/entrypoint.sh
@@ -2,5 +2,12 @@
 
 echo "ReadFileContent $1"
 echo "some custom text for testing"
+
+yourfilenames=`ls`
+for eachfile in $yourfilenames
+do
+   echo $eachfile
+done
+
 value="my_package.txt"
 echo "::set-output name=surname::$value"
