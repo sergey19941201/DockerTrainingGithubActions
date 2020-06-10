92e2a282e476c481fd0b624c2104924e0562cd56
Serhii <Koshelnyk>
2020-06-10 18:28:48 +0300

    read content

--- FileReadAction/entrypoint.sh
+++ FileReadAction/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
 echo "ReadFileContent $1"
-echo "::set-output name=surname::(Get-Content .\MobileAccess\MobileAccess.Droid\Properties\AndroidManifest.xml)"
+echo "::set-output name=surname::{Get-Content .\MobileAccess\MobileAccess.Droid\Properties\AndroidManifest.xml}"
