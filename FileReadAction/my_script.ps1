7ef8bbd389794503d3bf7c539218c01cbda4f4be
Serhii <Koshelnyk>
2020-06-11 11:38:45 +0300

    add .ps1

---
+++ FileReadAction/my_script.ps1
@@ -0,0 +1,15 @@
+##############################################
+#Script Title: Download File PowerShell Tool
+#Script File Name: Download-File.ps1 
+#Author: Ron Ratzlaff  
+#Date Created: 4/21/2016 
+##############################################
+
+#Requires -Version 3.0
+"Hello, World!wwwwww"
+
+Function Download-File
+{
+   "Hello, World!yyyynnnn"
+
+}
