c362460f60f1a6df8e44be2a51a8477b7864c72f
Sergey Koshelnyk <serhii.koshelnyk@extendaretail.com>
2020-07-24 13:07:50 +0300

    Update ShouldCreateNewRcBranchCheckScript.ps1

--- Scripts/ShouldCreateNewRcBranchCheckScript.ps1
+++ Scripts/ShouldCreateNewRcBranchCheckScript.ps1
@@ -2,18 +2,10 @@ param($branch_name)
 
 function CheckIfBranchExists()
 {
-    $branch_name
-
     [string]$branchList = git branch -a
-
-
-    "type list" 
-    $branchList
-    "end list"
 
     if($branchList -Match $branch_name)
     {
-        "FALSE"
         $Env:BUILD_SHOULD_CREATE_NEW_RC_OUTPUT = "false"
 
         return "branch exists";
